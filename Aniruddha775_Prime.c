641cff36a09ec26aeec9390fe4b422218016c8bc
Avijit2002 <89923957+Avijit2002@users.noreply.github.com>
2021-10-04 21:54:04 +0530

    Updated Aniruddha775_Prime.c
    Initialized the value of "i" outside for loop.

--- Aniruddha775_Prime.c
+++ Aniruddha775_Prime.c
@@ -1,15 +1,14 @@
 #include <stdio.h> 
 
 int main() {
-  int n, i, c = 0;
+  int n, i = 1, c = 0;
   printf("Enter any number ");
   scanf("%d", &n);
-  for (i = 1; i <= n; i++) {
+  for ( ; i <= n; i++) {
       if (n % i == 0) {
          c++;
       }
   }
-
   if (c == 2) {
   printf("It is a Prime number");
   }
