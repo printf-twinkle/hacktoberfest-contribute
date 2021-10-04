10d6af94e69b8633c2f689514fb6c18f1152c953
kmr-sahil <91898837+kmr-sahil@users.noreply.github.com>
2021-10-04 21:00:26 +0530

    create even_odd.java
    This program is in java to find whether the number is even or odd.

---
+++ even_odd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class prime
+{
+public static void main()
+{
+Scanner sc=new Scanner(System.in);
+int n=0;
+  System.out.println("Enter the Number");
+n=sc.nextInt();
+if(n%2==0)
+{
+System.out.println("The Number is even");
+}
+else
+{
+System.out.println("The Number is even");
+}
+}
+}
