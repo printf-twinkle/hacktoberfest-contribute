a709dedc2e559954056d9dc8aa43cd7826031b94
0anubhav0 <91758623+0anubhav0@users.noreply.github.com>
2021-10-04 20:58:37 +0530

    FactorialFinder.java
    a simple program in java to find the factorial of a number

---
+++ FcatorialFinder.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FactorialFinder{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    System.out.println(factorial(n));
+  }
+  static int factorial(int n){
+    int res=1;
+    if(n==0) return 1;
+    for(int i=1;i<=n;i++){
+       res*=i;
+    }
+    return res;
+  }
+}
