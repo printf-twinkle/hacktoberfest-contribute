7a31529b8dfef39039def33c715f8c6094dc68aa
Kanailal Manna <91842029+KanailalManna@users.noreply.github.com>
2021-10-05 00:36:01 +0530

    Create prime or not .java

---
+++ prime or not .java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class prime{
+
+    public static void main(String args[]) {
+
+        Scanner scan = new Scanner(System.in);
+        int number;
+        int count=0;
+        int i;
+
+
+        System.out.print("Enter a Number you want to check : ");
+        number = scan.nextInt();
+
+        for(i=2; i<number; i++)
+        {
+            if(number%i == 0)
+            {
+                count++;
+                break;
+            }
+        }
+        if(count == 0)
+        {
+            System.out.print("This is a prime number");
+        }
+        else
+        {
+            System.out.print("This is not a prime number");
+        }
+    }
+} 
