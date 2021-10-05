1baed59c1505afaeb6baf7d5b3571c56496ea982
Sahel <52469898+CoderSahel@users.noreply.github.com>
2021-10-05 14:20:22 +0530

    #123 solved

---
+++ palindrome.java
@@ -0,0 +1,22 @@
+//JAVA program to check palindrome number
+public class Palindrome
+{  
+  public static void main(String args[])
+  {  
+
+    int r,sum=0,temp;    
+    int n=454;//It is the number variable to be checked for palindrome  
+
+    temp=n;    
+    while( n>0 )
+    {    
+      r=n%10;  //getting remainder  
+      sum=(sum*10)+r;    
+      n=n/10;    
+    }    
+    if(temp==sum)    
+    System.out.println("palindrome number ");    
+    else    
+    System.out.println("not palindrome");    
+  }  
+}  
