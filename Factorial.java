1c68803bfe9675a1c2cf4b17fae5d5d20b606d28
Sahel <52469898+CoderSahel@users.noreply.github.com>
2021-10-05 16:42:39 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,8 +1,10 @@
+// Java program to find factorial of a number
 import java.util.Scanner;
 
-public class Factorial{
+public class Factorial
+{
   public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
     int n=sc.nextInt();
     System.out.println(factorial(n));
   }
