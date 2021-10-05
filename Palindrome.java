5c911381114193b988dcd04f343ffcfefc756282
sanjana konte <konte.sanjana@gmail.com>
2021-10-05 11:22:52 +0530

    A java program to find palindrome of a given number

---
+++ Palindrome.java
@@ -0,0 +1,27 @@
+class Palindrome {
+    public boolean isPalindrome(int x) {
+        int rem=0, sum = 0;
+        int temp = x;
+        if(temp<0)
+            return false;
+        while(x!=0){
+            rem = x%10;
+            x = x/10;
+            sum = (sum*10) + rem;
+        }
+        if (sum == temp){
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter input");
+        int num = input.nextInt();
+        Solution S = new Solution();
+        S.isPalindrome(num);
+        
+    }
+}
