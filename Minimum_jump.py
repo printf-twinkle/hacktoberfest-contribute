44512b699c2438391fbfebfd472b9f01db807b94
Ankush <91725844+Ankushkr14@users.noreply.github.com>
2021-10-05 07:21:16 +0530

    Minimum number of jump.py

---
+++ Minimum_jump.py
@@ -0,0 +1,42 @@
+def minJumps(arr, n):
+  
+  if (n <= 1):
+    return 0
+
+  if (arr[0] == 0):
+    return -1
+  maxReach = arr[0] 
+  step = arr[0]
+  jump = 1
+  
+  
+  for i in range(1, n):
+
+    if (i == n-1):
+      return jump
+  
+    maxReach = max(maxReach, i + arr[i])
+
+    step -= 1;
+  
+    if (step == 0):
+   
+      jump += 1
+     
+      if(i >= maxReach):
+        return -1
+    
+      step = maxReach - i;
+  return -1
+  
+lst = []
+n = int(input("Enter number of elements : "))
+
+for i in range(0, n):
+    ele = int(input())
+ 
+    lst.append(ele)
+     
+print(lst)
+size = len(arr)  
+print("Minimum number of jumps to reach end is % d " % minJumps(arr, size))
