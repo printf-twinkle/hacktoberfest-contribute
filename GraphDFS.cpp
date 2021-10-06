7ff1221460bddb6da6c9c1bda666b7ff806b6033
arbaztalk2me <72205785+arbaztalk2me@users.noreply.github.com>
2021-10-06 19:55:01 +0530

    Create GraphDFS.cpp

---
+++ GraphDFS.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+
+using namespace std;
+
+void printDFS(int** edges,int n,int sv,bool* visited){
+    cout<<sv<<endl;
+    visited[sv]=true;
+    for(int i=0;i<n;i++){
+        if(i==sv){
+            continue;
+        }
+        if(edges[sv][i]==1){
+            if(visited[i]){
+                continue;
+            }
+            printDFS(edges,n,i,visited);
+        }
+    }
+}
+
+
+int main(){
+    int n,e;
+    cin>>n>>e;
+    int** edges=new int*[n];
+    for(int i=0;i<n;i++){
+        edges[i]=new int[n];
+        for(int j=0;j<n;j++){
+            edges[i][j]=0;
+        }
+    }
+
+    for(int i=0;i<e;i++){
+        int f,s;
+        cin>>f>>s;
+        edges[f][s]=1;
+        edges[s][f]=1;
+    }
+
+    bool* visited=new bool[n];
+
+    for(int i=0;i<n;i++){
+        visited[i]=false;
+    }
+
+   printDFS(edges,n,0,visited);
+
+
+    delete [] visited;
+
+    for(int i=0;i<n;i++){
+         delete [] edges[i];
+    }
+    delete [] edges;
+
+}
